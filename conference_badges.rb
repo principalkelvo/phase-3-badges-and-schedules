3762807a2d1864ad9323a93da2252a091eb0c20d
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-08-20 17:30:30 +0300

    commit tree 2b88d0a8e1255042d88720032917f4c9acd1714d

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator badges
+    badges.map do |badge|
+        "Hello, my name is #{badge}."
+    end
+end
+
+def assign_rooms names
+    names.each_with_index do |value, index|
+        "Hello, #{value}! You'll be assigned to room #{index}!"
+    end
+end
